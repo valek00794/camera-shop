3ef10e3d55d453e835e8a70a732c4eca5ea212bf
Valentin <valentin_00794@bk.ru>
2023-02-26 00:41:08 +0300

    Переход по списку поиска стрелками

--- project/src/components/form-search/form-search.tsx
+++ project/src/components/form-search/form-search.tsx
@@ -1,5 +1,5 @@
 import classnames from 'classnames';
-import { ChangeEvent, useEffect, useState } from 'react';
+import { ChangeEvent, useEffect, useRef, useState } from 'react';
 import { Link } from 'react-router-dom';
 
 import { useAppDispatch, useAppSelector } from '../../hooks';
@@ -9,23 +9,87 @@ import { getCameraTitle } from '../../utils/utils';
 
 const defaultInputSearchString = '';
 
+
 function FormSearch(): JSX.Element {
   const dispatch = useAppDispatch();
   const [searchName, setSearchName] = useState(defaultInputSearchString);
   const [isOpenSelectList, setIsOpenSelectList] = useState(false);
   const foundCameras = useAppSelector(getFoundCameras);
+  const availableSelectListElements: HTMLAnchorElement[] | null[] = [];
+  const availableElementsRef = useRef(availableSelectListElements);
+  foundCameras?.length ? availableElementsRef.current.length = foundCameras?.length : availableElementsRef.current.length = 0;
+  const startActiveElement = document.querySelector('.form-search__input') as HTMLAnchorElement;
+  const endActiveElement = document.querySelector('.form-search__reset') as HTMLAnchorElement;
 
   useEffect(() => {
+    let startIndex = -1;
+    document.body.classList.toggle('modal-open', isOpenSelectList);
     let isMounted = true;
+
+    const handleKeyPress = (evt: KeyboardEvent) => {
+      const currntActiveElement = document.activeElement as HTMLAnchorElement;
+
+      if (evt.key === 'Escape') {
+        handleResetSearch();
+      }
+
+      if (evt.key === 'Tab') {
+        if (!evt.shiftKey) {
+          if (currntActiveElement !== endActiveElement) {
+            startIndex = availableElementsRef.current.findIndex((element) => element === currntActiveElement);
+          }
+        }
+      }
+      if (evt.key === 'Tab') {
+        if (evt.shiftKey) {
+          startIndex = availableElementsRef.current.findIndex((element) => element === currntActiveElement) - 1;
+        }
+      }
+      if (evt.key === 'ArrowUp' &&
+        availableElementsRef.current[startIndex] !== null &&
+        startIndex >= 0) {
+        if (startIndex === 0) {
+          startActiveElement.focus();
+          startIndex = -1;
+        }
+        if (startIndex > 0) {
+          startIndex = startIndex - 1;
+          availableElementsRef.current[startIndex]?.focus();
+        }
+      }
+      if (evt.key === 'ArrowDown' &&
+        availableElementsRef.current[startIndex] !== null &&
+        startIndex <= availableElementsRef.current.length) {
+        if (startIndex === availableElementsRef.current.length - 1) {
+          endActiveElement.focus();
+          startIndex = startIndex + 1;
+        }
+        if (startIndex < availableElementsRef.current.length - 1) {
+          startIndex = startIndex + 1;
+          availableElementsRef.current[startIndex]?.focus();
+        }
+      }
+    };
+
+    const handleCloseSelectList = (evt: MouseEvent) => {
+      if (evt.target !== startActiveElement) {
+        handleResetSearch();
+      }
+    };
+
+    window.addEventListener('click', handleCloseSelectList);
     if (isMounted && searchName.trim() !== defaultInputSearchString && foundCameras?.length !== 0) {
       setIsOpenSelectList(true);
+      window.addEventListener('keyup', handleKeyPress);
     } else {
       setIsOpenSelectList(false);
     }
     return () => {
       isMounted = false;
+      window.removeEventListener('keyup', handleKeyPress);
+      window.removeEventListener('click', handleCloseSelectList);
     };
-  }, [dispatch, foundCameras?.length, searchName]);
+  }, [dispatch, endActiveElement, foundCameras?.length, isOpenSelectList, searchName, startActiveElement]);
 
   const handleInputSearch = (evt: ChangeEvent<HTMLInputElement>) => {
     setSearchName(evt.target.value);
@@ -49,26 +113,35 @@ function FormSearch(): JSX.Element {
           <svg className="form-search__icon" width="16" height="16" aria-hidden="true">
             <use xlinkHref="#icon-lens"></use>
           </svg>
-          <input className="form-search__input" type="text" autoComplete="off" placeholder="Поиск по сайту" value={searchName} onChange={handleInputSearch} />
+          <input
+            className="form-search__input"
+            type="text"
+            autoComplete="off"
+            placeholder="Поиск по сайту"
+            value={searchName}
+            onChange={handleInputSearch}
+            onFocus={(evt) => evt.target.select()}
+          />
         </label>
         <ul className="form-search__select-list scroller">
 
           {
             foundCameras?.length !== 0 &&
-                foundCameras?.map((camera, index) => (
-                  <Link
-                    key={camera.id}
-                    to={`/catalog/${camera?.id}/description`}
-                    onClick={handleResetSearch}
-                  >
-                    <li
-                      className="form-search__select-item"
-                      tabIndex={index + 1}
-                    >
-                      {getCameraTitle(camera)}
-                    </li>
-                  </Link>
-                ))
+            foundCameras?.map((camera, index) => (
+              <Link
+                key={camera.id}
+                tabIndex={0}
+                to={`/catalog/${camera?.id}/description`}
+                onClick={handleResetSearch}
+                ref={(element) => {
+                  availableElementsRef.current[index] = element;
+                }}
+              >
+                <li className="form-search__select-item">
+                  {getCameraTitle(camera)}
+                </li>
+              </Link>
+            ))
           }
 
         </ul>
